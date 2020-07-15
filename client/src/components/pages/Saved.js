f516c76c6a5554c8c4c606607dcaf13ef53ed23b
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-07-15 17:49:24 -0500

    heroku deployment

--- client/src/components/pages/Saved.js
+++ client/src/components/pages/Saved.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function Saved() {
-    return (
-        <React.Fragment>
-            <h1>Saved Books</h1>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <h1>Saved Books</h1>
+    </React.Fragment>
+  );
 }
